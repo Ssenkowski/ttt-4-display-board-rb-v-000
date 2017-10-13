3819c27c00fa6515ff10da8f395d493b5aac4561
Ssenkowski <scsenkowski@gmail.com>
2017-10-13 13:24:46 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -8,6 +8,7 @@ puts "-----------"
 puts " #{board(6)}  | #{board(7)}  | #{board(8)}  "
 end
 
+board = [" "," "," "," ","X"," "," "," "," "]
+
 display_board(board)
 
-board = [" "," "," "," ","X"," "," "," "," "]
